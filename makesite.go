5450cd91f209f5b74d9df0d25a852b53ed8816b6
Panda64 <JaylenSchelb@yahoo.com>
2022-02-10 00:40:52 -0800

    v0.2

--- makesite.go
+++ makesite.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// Page holds all the information we need to generate a new
+// HTML page from a text file on the filesystem.
+type Page struct {
+	HTMLPagePath string
+	Content      string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	// Reading entire file content
+	content, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	page := Page{
+		// TextFilePath: "/",
+		// TextFileName: "first-post.txt",
+		HTMLPagePath: "first-post.html",
+		Content:      string(content),
+	}
+
+	// Create a new template in memory named "template.tmpl".
+	// When the template is executed, it will parse template.tmpl,
+	// looking for {{ }} where we can inject content.
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	// Create a new, blank HTML file.
+	newFile, err := os.Create(page.HTMLPagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Executing the template injects the Page instance's data,
+	// allowing us to render the content.
+	// Furthermore, upon execution, the rendered template will be
+	// saved inside the new file we created earlier.
+	t.Execute(newFile, page)
+
+	// Printing page content
+	fmt.Println(page.Content)
 }
